0223e5b984bc656ae4b021a067db438aaa4cbfa7
Rittick Datta <drittick@gmail.com>
2024-04-02 14:37:53 +0200

    Website responsiveness: add media queries

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -4,3 +4,9 @@ const aside = document.getElementById("summaryCard");
 button.addEventListener("click", () => {
   aside.classList.toggle("showHideAside");
 });
+
+function toggleHamburgerMenu() {
+  const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
+
+  hamburgerMenu.classList.toggle("")
+}
