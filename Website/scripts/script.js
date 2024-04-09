21e3fe63354db4f7af0336cdcdbecb724b910740
Rittick Datta <drittick@gmail.com>
2024-04-09 15:39:59 +0200

    Edit frontend and backend skill blocks

--- Website/scripts/script.js
+++ Website/scripts/script.js
@@ -1,6 +1,10 @@
 const button = document.getElementById("showSummary");
 const aside = document.getElementById("summaryCard");
 
+const skillCards = document.querySelectorAll(".skill-card");
+const years = document.querySelectorAll(".skill-experience");
+const recentlyUsed = document.querySelectorAll(".skill-recently-used");
+
 button.addEventListener("click", () => {
   aside.classList.toggle("showHideAside");
 });
@@ -8,5 +12,18 @@ button.addEventListener("click", () => {
 function toggleHamburgerMenu() {
   const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
 
-  hamburgerMenu.classList.toggle("")
+  hamburgerMenu.classList.toggle("");
+}
+
+for (let i = 0; i < skillCards.length; i++) {
+  const card = skillCards[i];
+  card.addEventListener("mouseover", () => {
+    years[i].style.display = "block";
+    recentlyUsed[i].style.display = "block";
+  });
+
+  card.addEventListener("mouseout", () => {
+    years[i].style.display = "none";
+    recentlyUsed[i].style.display = "none";
+  });
 }
